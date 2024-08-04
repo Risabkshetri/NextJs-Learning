ea6008251ab05c77c325e5a97973b4a44ad6f310
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-08-04 09:55:04 +0530

    music-project design finished

---
+++ music-school/src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+
+
+function page() {
+  return (
+    <div className="min-hsceen bg-black py-12  pt-36">
+        <h1 className="text-lg md:text-6xl text-white font-bold font-sans text-center">Contact Us</h1>
+    </div>
+  )
+}
+
+export default page
--- music-school/src/app/page.tsx
+++ music-school/src/app/page.tsx
@@ -1,5 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
@@ -9,6 +13,10 @@ export default function Home() {
   <HeroSection />
   <FeaturedCourses />
   <WhyChooseUs />
+  <MusicSchoolTestimonials />
+  <UpcomingWebinars />
+  <Instructors />
+  <Footer />
   </div>
   );
 }
