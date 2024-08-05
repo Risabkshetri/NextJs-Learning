a9c7f0f4d1c3696fa39636b5a15ee5643d51dee6
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-08-05 10:11:06 +0530

    deployment error fixed

--- music-school/src/app/courses/page.tsx
+++ music-school/src/app/courses/page.tsx
@@ -1,79 +1,76 @@
-'use client';
+"use client";
 import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "../../components/ui/3d-card";
-import courseData from '@/data/music_courses.json'
+import courseData from "@/data/music_courses.json";
 import Link from "next/link";
 
 function page() {
-
-  interface Course{
-    id: number,
-    title: string,
-    slug: string,
-    description: string,
-    price: number,
-    instructor: string,
-    isFeatured: boolean,
-    image: string
-        
-}
+  interface Course {
+    id: number;
+    title: string;
+    slug: string;
+    description: string;
+    price: number;
+    instructor: string;
+    isFeatured: boolean;
+    image: string;
+  }
   return (
     <div className="min-hsceen bg-black py-12  pt-36">
-        <h1 className="text-lg md:text-6xl font-bold text-white mb-8 font-sans text-center ">All Courses({courseData.courses.length})</h1>
+      <h1 className="text-lg md:text-6xl font-bold text-white mb-8 font-sans text-center ">
+        All Courses({courseData.courses.length})
+      </h1>
 
-        <div className="flex flex-wrap gap-8 justify-center">
-             {courseData.courses.map((course:Course)=> (
-                    <CardContainer className="inter-var" key={course.id}>
-                    <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-                      <CardItem
-                        translateZ="50"
-                        className="text-xl font-bold text-neutral-600 dark:text-white"
-                      >
-                        {course.title}
-                      </CardItem>
-                      <CardItem
-                        as="p"
-                        translateZ="60"
-                        className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                      >
-                        {course.description}
-                      </CardItem>
-                      <CardItem translateZ="100" className="w-full mt-4">
-                        <Image
-                          src={course.image}
-                          height="1000"
-                          width="1000"
-                          className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                          alt={course.title}
-                        />
-                      </CardItem>
-                      <div className="flex justify-between items-center mt-20">
-                        <CardItem
-                          translateZ={20}
-                          as={Link}
-                          href="https://twitter.com/mannupaaji"
-                          target="__blank"
-                          className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                        >
-                          Try now →
-                        </CardItem>
-                        <CardItem
-                          translateZ={20}
-                          as="button"
-                          className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                        >
-                          Sign up
-                        </CardItem>
-                      </div>
-                    </CardBody>
-                  </CardContainer>
-              
-             ))}
-
-        </div>
+      <div className="flex flex-wrap gap-8 justify-center">
+        {courseData.courses.map((course: Course) => (
+          <CardContainer className="inter-var" key={course.id}>
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white"
+              >
+                {course.title}
+              </CardItem>
+              <CardItem
+                as="p"
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              >
+                {course.description}
+              </CardItem>
+              <CardItem translateZ="100" className="w-full mt-4">
+                <Image
+                  src={course.image}
+                  height="1000"
+                  width="1000"
+                  className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  alt={course.title}
+                />
+              </CardItem>
+              <div className="flex justify-between items-center mt-20">
+                <Link href={`/courses/${course.slug}`} target="_blank">
+                  <CardItem
+                    translateZ={20}
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Try now →
+                  </CardItem>
+                </Link>
+                <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                >
+                  Sign up
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
