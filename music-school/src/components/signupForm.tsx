70c6d6bc5f431562d225707167f190cb158cee98
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-08-10 13:00:09 +0530

    unser experience optimized in signup form

--- music-school/src/components/signupForm.tsx
+++ music-school/src/components/signupForm.tsx
@@ -14,6 +14,10 @@ export function SignupFormDemo() {
     e.preventDefault();
     console.log("Form submitted");
   };
+
+ const handleClick = () => {
+   alert("Thank you for contacting us");
+ }
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black shadow-black/5 dark:shadow-white/5 mt-12">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
@@ -51,8 +55,9 @@ export function SignupFormDemo() {
             type="twitterpassword"
           />
         </LabelInputContainer>
-
+        
         <button
+          onClick={handleClick}
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
